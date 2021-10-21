6f034f06169ae0b7b53a49b5026d2c1ec78d3598
rossluu <91068108+rossluu@users.noreply.github.com>
2021-10-21 11:09:38 +0200

    Register/Login toggle complete
    Finished the register and login toggle. added the css for login screen for desktop, ipad and iphone and then used jquery to show and hide. function can still be improved

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
     console.log("jQuery Added and Ready (index.js)");
 
+    //LOGIN & REGISTER TOGGLE
+
+    $(".login-slider-all").click(function() {
+        $(".reg-all").hide(600)
+        $(".login-box-all").show(600)
+    })
+
+    $(".reg-slider-all").click(function() {
+        $(".login-box-all").hide(600)
+        $(".reg-all").show(600)
+    })
+
+
 })
