aba0553cc3ee75a6cfb6f6bd2bbb1b03628f1e62
rossluu <91068108+rossluu@users.noreply.github.com>
2021-10-22 15:59:02 +0200

    Login Authentication & home page functionality
    Added login security by adding authentication functionality. Used session storage to connect the users name to the home page. Added extra user paths in the home page to allow user to logout and switch to nightmode.

--- js/index.js
+++ js/index.js
@@ -4,14 +4,74 @@ $(document).ready(function() {
     //LOGIN & REGISTER TOGGLE
 
     $(".login-slider-all").click(function() {
-        $(".reg-all").hide(600)
-        $(".login-box-all").show(600)
+        $(".reg-all").slideUp(600)
+        $(".login-box-all").slideDown(600)
     })
 
     $(".reg-slider-all").click(function() {
-        $(".login-box-all").hide(600)
-        $(".reg-all").show(600)
+        $(".login-box-all").slideUp(600)
+        $(".reg-all").slideDown(600)
     })
 
+    //LOGIN AUTHENTICATION
+
+ 
+    $(".submit-log").click(function() {
+
+        var username = $(".username").val();
+        var password = $(".password").val();
+
+
+   url = "https://owmakerspace.co.za/users/data.json";
+
+    $.getJSON(url, function(result){
+        console.log(result.users)
+
+        for(i = 0; i < result.users.length; i++){
+            if(result.users[i].username === username){
+             
+                if(result.users[i].password === password){
+                    
+                    if(result.users[i].account === "active"){
+                        window.location.href = "../index.html";
+                        sessionStorage.setItem("username", username);
+                    } else {
+                        $(".auth-message").slideDown(200);
+                        $(".auth-message").text("Your account has been suspended")
+                    }
+
+                } else {
+                    $(".auth-message").slideDown(200);
+                    $(".auth-message").text("Your Username or Password do not match")
+                }
+
+
+            } else {
+                $(".auth-message").slideDown(200);
+                $(".auth-message").text("Your Username or Password do not match");
+            }
+        }
+
+
+
+
+
+
+    }); //end of http request
+
+    }); //end of login click event
+
+    $(".name_out").text(sessionStorage.getItem("username"));
+
+
+
+    $(".profile-pic-box").click(function() {
+       $(".options-block").show(600) 
+    })
+
+    $(".options-block").mouseleave(function(){
+        $(this).hide(600)
+    }) 
+ 
 
 })
