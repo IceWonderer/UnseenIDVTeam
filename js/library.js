1d1d31dabe9bcaef3d71d09240834e3dd0c33352
T2sha <21100474@virtualwindow.co.za>
2021-10-29 13:55:45 +0200

    Individual page
    Added functionality to the individual page as well as the movie trailer

--- js/library.js
+++ js/library.js
@@ -42,6 +42,8 @@ $(document).ready(function() {
 
 
 
+
+
             $(".top-movies-box").append(
                 ` <a href=../pages/individual_movie.html?id=` + popular.results[i] + `><div class = 'top-movie' ><img src=` + CoverImage + ` 'alt=' 'class= 'topMovieImage'>
                <div class="movie-overlay">
@@ -123,44 +125,30 @@ $(document).ready(function() {
             var IBG_backdrop_path = 'https://image.tmdb.org/t/p/w500/' + popular.results[i].backdrop_path
             var CoverImage = 'https://image.tmdb.org/t/p/w500/' + popular.results[i].poster_path
 
-
             $(".top-movies-box-pd").append(
-                ` <a href=../pages/individual_movie.html?id=` + popular.results[i] + `><div class = 'top-movie ' ><img src=` + CoverImage + ` 'alt=' 'class= 'topMovieImage'>
-               </div></a> `
+                "<div class = 'top-movie-pd' ><img src='" + CoverImage + "'alt=' 'class= 'topMovieImage-pd'></div>"
             );
-
-
-
         }
     });
 
     ////////////////////////////////////////////////////////////////////////////////////////////////start of movie library PD
-    for (var i = 1; i < 10; i++){
-        const url2 = "https://api.themoviedb.org/3/movie/popular?api_key=0bf5587914a4404af70b0748372ba59f&language=en-US&page=" + i;
-    
-
-
-
-    $.getJSON(url2, function(movies) {
+    $.getJSON(url, function(popular) {
 
 
         function getRndInteger(min, max) {
             return Math.floor(Math.random() * (max - min)) + min;
         }
 
-        for (i = 6; i <= getRndInteger(15, 15); i++) {
+        for (i = 6; i <= getRndInteger(11, 11); i++) {
 
-            var IBG_backdrop_path = 'https://image.tmdb.org/t/p/w500/' + movies.results[i].backdrop_path
-            var CoverImage = 'https://image.tmdb.org/t/p/w500/' + movies.results[i].poster_path
+            var IBG_backdrop_path = 'https://image.tmdb.org/t/p/w500/' + popular.results[i].backdrop_path
+            var CoverImage = 'https://image.tmdb.org/t/p/w500/' + popular.results[i].poster_path
 
             $(".movie-container-pd").append(
-                `  <a href=../pages/individual_movie.html?id=` + movies.results[i] + `><div  class = 'movie-pd  ${movies.results[i].genre_ids[0]}'   ><img src=` + CoverImage + ` 'alt=' 'class= 'topMovieImage-pd'></div></a> `
-                );
+                "<div class = 'movie-pd' ><img src='" + CoverImage + "'alt=' 'class= 'topMovieImage-pd'></div>"
+            );
         }
     });
-    
-    }
-
     ////////////////////////////////////////////////////////////////////////////////////////////////end of movie library PD
 
     ////////////////////////////////////////////////////////////////////////////////////////////////start of movie library Phone
@@ -181,38 +169,5 @@ $(document).ready(function() {
             );
         }
     });
-
-
-
-
-
-    for (var i = 1; i < 10; i++){
-        const url2 = "https://api.themoviedb.org/3/movie/popular?api_key=0bf5587914a4404af70b0748372ba59f&language=en-US&page=" + i;
-    
-
-
-
-    $.getJSON(url2, function(movies) {
-
-
-        function getRndInteger(min, max) {
-            return Math.floor(Math.random() * (max - min)) + min;
-        }
-
-        for (i = 6; i <= getRndInteger(15, 15); i++) {
-
-            var IBG_backdrop_path = 'https://image.tmdb.org/t/p/w500/' + movies.results[i].backdrop_path
-            var CoverImage = 'https://image.tmdb.org/t/p/w500/' + movies.results[i].poster_path
-
-            $(".movie-container-mb").append(
-                `  <a href=../pages/individual_movie.html?id=` + movies.results[i] + `><div  class = 'movie-mb  ${movies.results[i].genre_ids[0]}'   ><img src=` + CoverImage + ` 'alt=' 'class= 'topMovieImage-mb'></div></a> `
-                );
-        }
-    });
-    
-    }
-
-
-
     ////////////////////////////////////////////////////////////////////////////////////////////////end of movie library Phone
 });
